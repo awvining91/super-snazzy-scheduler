5654ccd436e98dc01242418e31c4d32fdb46f874
Will Vining <awvining91@gmail.com>
2022-08-30 17:45:16 -0700

    added basic local storage example in html and javascript

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,26 @@ var timeDisplayEl = $('#time-display');
 
 
 
+// saving text in local storage
+const input = document.querySelector("input"),
+    h3 = document.querySelector("h3");
+    
+h3.innerHTML = localStorage.getItem("value");
+
+input.addEventListener("keyup", display);
+
+//stores in the info on local browser
+function display () {
+  localStorage.setItem('value', input.value);
+//gets stored info from local browser
+  console.log(localStorage.getItem("value"));
+  h3.innerHTML = localStorage.getItem("value");
+
+}
+
+
+
+
 
 
 
@@ -11,6 +31,12 @@ function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
   }
+
+
+
+  
+
+
 
 
 
