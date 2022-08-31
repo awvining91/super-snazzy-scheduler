c4f014a519e1b76069fb254bd8355cc01194db06
Will Vining <awvining91@gmail.com>
2022-08-30 23:57:58 -0700

    attempted adding logic for color time-blocks

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,30 @@
 // save reference to important DOM elements
 var timeDisplayEl = $('#time-display');
 
+//attempt jquery logic for time color blocks
 
+
+$(".time-block").each(function () {
+  var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+  console.log( blockHour, currentHour)
+
+  //check if we've moved past this time, click into css/html given classes of past, present, or future
+  if (blockHour < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+  }
+  else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+  }
+  else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+  }
+})
 
 
 
@@ -55,14 +78,32 @@ $(".saveBtn").on("click", function () {
 
 
   console.log(this);
+
   var text = $(this).siblings(".description").val();
+
   var time = $(this).parent().attr("id");
 
   localStorage.setItem(time, text);
 
 })
 
-$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#9 .description").val(localStorage.getItem("9"));
+
+$("#10 .description").val(localStorage.getItem("10"));
+
+$("#11 .description").val(localStorage.getItem("11"));
+
+$("#12 .description").val(localStorage.getItem("12"));
+
+$("#13 .description").val(localStorage.getItem("13"));
+
+$("#14 .description").val(localStorage.getItem("14"));
+
+$("#15 .description").val(localStorage.getItem("15"));
+
+$("#16 .description").val(localStorage.getItem("16"));
+
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 
