871741edbb8c5fe79c05ddec36196fd423e242a0
Will Vining <awvining91@gmail.com>
2022-08-31 17:18:20 -0700

    modified variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 
 
-// save reference to important DOM elements
+// This helps display the date and time. I used a the mini-project as reference.
 var timeDisplayEl = $('#time-display');
 
 //attempt jquery logic for time color blocks
@@ -18,37 +18,41 @@ $(".saveBtn").on("click", function () {
 
   console.log(this);
 
-  var text = $(this).siblings(".description").val();
+  var scribble = $(this).siblings(".write-here").val();
 
-  var time = $(this).parent().attr("id");
+  var happen = $(this).parent().attr("id");
 
-  localStorage.setItem(time, text);
+  localStorage.setItem(happen, scribble);
 
 })
 
-$("#9 .description").val(localStorage.getItem("9"));
+$("#9 .write-here").val(localStorage.getItem("9"));
 
-$("#10 .description").val(localStorage.getItem("10"));
+$("#10 .write-here").val(localStorage.getItem("10"));
 
-$("#11 .description").val(localStorage.getItem("11"));
+$("#11 .write-here").val(localStorage.getItem("11"));
 
-$("#12 .description").val(localStorage.getItem("12"));
+$("#12 .write-here").val(localStorage.getItem("12"));
 
-$("#13 .description").val(localStorage.getItem("13"));
+$("#13 .write-here").val(localStorage.getItem("13"));
 
-$("#14 .description").val(localStorage.getItem("14"));
+$("#14 .write-here").val(localStorage.getItem("14"));
 
-$("#15 .description").val(localStorage.getItem("15"));
+$("#15 .write-here").val(localStorage.getItem("15"));
 
-$("#16 .description").val(localStorage.getItem("16"));
+$("#16 .write-here").val(localStorage.getItem("16"));
 
-$("#17 .description").val(localStorage.getItem("17"));
+$("#17 .write-here").val(localStorage.getItem("17"));
+
+$("#18 .write-here").val(localStorage.getItem("18"));
+
+$("#19 .write-here").val(localStorage.getItem("19"));
 
 
 
 
 
-// handle displaying the time
+// This is a function for displaying date and time
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
@@ -57,8 +61,6 @@ function displayTime() {
 
 
   
-
-
 
 
   setInterval(displayTime, 1000);
