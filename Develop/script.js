62214bad77379aa093799d5a03ec7a9579179392
Will Vining <awvining91@gmail.com>
2022-08-31 17:40:22 -0700

    added a lot of comments

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,18 @@ var timeDisplayEl = $('#time-display');
 
 
 
-//attempting local storage logic
+/*  Here is the local storage section. Basically I had jQuery grab the .saveBtn class
+to look for a click, which will then perfom a function that will pass through
+a "this" parameter that refers the the button tag element area.
+
+I had to two local variables defined. Scribble chains the .sibling method to
+grab the text written on the app hour block with the .write-here class and
+returns the value.
+
+The happen local variable grabs and passes the "this" parameter to look
+reference the parent id and stores them in local storage.
+
+*/
 
 
 $(".saveBtn").on("click", function () {
@@ -25,6 +36,21 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(happen, scribble);
 
 })
+
+
+/*   Here I used jQuery to grab the id and class to return
+the values stored in local storage with getItem.
+
+Bascially jQuery helps me return the text written in the time block
+area that was saved when the user pushed the saved icon and 
+returns it in the same spot. 
+
+For me writing this in
+vanilla javascript would have been really hard but
+with jQuery it's way easier for me. :)
+
+
+*/
 
 $("#9 .write-here").val(localStorage.getItem("9"));
 
@@ -64,3 +90,38 @@ function displayTime() {
 
 
   setInterval(displayTime, 1000);
+
+  /* Some of my many references/resources used:
+
+  https://api.jquery.com/
+
+  https://getbootstrap.com/docs/4.1/getting-started/introduction/
+
+  https://stackoverflow.com/questions/40791207/setting-and-getting-localstorage-with-jquery
+
+  https://demos.devexpress.com/Bootstrap/Scheduler/ViewTypes.aspx
+
+  https://www.w3schools.com/jquery/jquery_events.asp
+
+  https://www.youtube.com/watch?v=9YOVfiyNxhc
+
+  youtube.com
+
+  https://www.youtube.com/c/TheNetNinja/playlists?app=desktop
+
+  https://www.youtube.com/watch?v=6BozpmSjk-Y
+
+  lots of google
+
+  class materials
+
+  https://www.youtube.com/watch?v=TBVpCKNuPtw
+
+  class miniproject
+
+  some others I don't remember the links to
+
+
+  */
+
+  
